75cb22c44806cd9b63234d6dc3afdfdc6bcc4974
dustinheestand <heestand@post.harvard.edu>
2019-04-12 09:09:40 -0500

    commented eight queens

--- myCode/prolog/eight_queens.pl
+++ myCode/prolog/eight_queens.pl
@@ -1,7 +1,7 @@
 :- use_module(library(clpfd)).
-
+:- op(920, fy, *).
 % My solution
-% Makes 100x more inferences than the reference solution below
+% Makes 10interx more inferences than the reference solution below
 eight_queens(Solution) :-
     length(Solution, 8),
     maplist(length_list(2), Solution),
@@ -11,11 +11,11 @@ eight_queens(Solution) :-
     maplist(get_x, Solution, [1, 2, 3, 4, 5, 6, 7, 8]),
     maplist(get_y, Solution, Ys),
     all_distinct(Ys),
-    label(Ys),
     maplist(get_diag1, Solution, D1s),
     all_distinct(D1s),
     maplist(get_diag2, Solution, D2s),
-    all_distinct(D2s).
+    all_distinct(D2s),
+    label(Ys).
 
 length_list(Length, List) :-
     length(List, Length).
@@ -27,23 +27,42 @@ get_diag1([X, Y], D) :-
 get_diag2([X, Y], D) :-
     D#=X-Y.
 
-
 % This is the provided solution code.
 n_queens(N, Qs) :-
     length(Qs, N),
     Qs ins 1..N,
-    safe_queens(Qs),
-    label(Qs).
-
-safe_queens([]).
-safe_queens([Q|Qs]) :-
-    safe_queens(Qs, Q, 1),
     safe_queens(Qs).
 
+% Once we've placed all the queens, we're good
+safe_queens([]).
+safe_queens([Q|Qs]) :-
+    % Constrain the rest of the Qs to be compatible with this one
+    safe_queens(Qs, Q, 1),
+    % Place the rest of the queens - we've already constrained all of them
+    % to be compatible with Q, so now they just all need to be compatible with
+    % one another, subject to that constraint
+    safe_queens(Qs).
+
+% Once we've constrained all the queens to the right, we're done
 safe_queens([], _, _).
 safe_queens([Q|Qs], Q0, D0) :-
+    % Constrain the Queen D0 rows over from the one we just placed
+    % So that it can't be in the same row.
     Q0#\=Q,
+    % D0 is the row difference between the two Q's
+    % Constrain the Queen D0 rows over from the one we just placed
+    % such that it can't be on the same diagonal, that is, D0 higher
+    % or D0 lower
     abs(Q0-Q)#\=D0,
+    % Now constrain the next queen in line
     D1#=D0+1,
     safe_queens(Qs, Q0, D1).
 
+% This is an easy system of equations that I was using to understand
+% how to properly use constraints
+table(TurtleOnTable, CatOnTable, N) :-
+    N#>=0,
+    max_list([TurtleOnTable, CatOnTable], Max),
+    N#=<Max,
+    N+C#=CatOnTable,
+    N-C#=TurtleOnTable.
